a41914f6ba1607cb391d6f0178c0fffc03d03f34
Navid Dezashibi <dezashibi@gmail.com>
2024-02-13 14:54:48 +0400

    init commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import App from './App.svelte';
+
+// Attempt to get the element
+const appElement = document.getElementById('app');
+
+// Check if the element is not found
+if (!appElement) {
+    throw new Error("Failed to find the element with ID 'app'");
+}
+
+// Since we've thrown an error if appElement is null, TypeScript knows appElement is not null here
+const app = new App({
+    target: appElement,
+});
+
+export default app;
